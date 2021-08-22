c1586d477f7fc0c519b4e384518c6ea3189574d6
foofx <fangxuan.foo@gmail.com>
2021-08-22 18:10:22 +0800

    Updated logic.js and index.html

--- static/js/logic.js
+++ static/js/logic.js
@@ -50,10 +50,58 @@ d3.select("#submit_this").on("click",()=>{
   var bedroom = document.getElementById("bedroom_range").value;
   var bathroom = document.getElementById("bathroom_range").value;
   var carspace = document.getElementById("carspaces_range").value;
-  var landsize = document.getElementById("land").value;
-  var builtsize = document.getElementById("built").value;
-  var builtyear= document.getElementById("builtdate").value;
+  var land = document.getElementById("land").value;
+  var built = document.getElementById("built").value;
+  var year= document.getElementById("builtdate").value;
   var suburb = document.getElementById("suburbs").value;
+  var modal = document.getElementById("myModal");
+  var tryagain = document.getElementsByClassName("close")[0];
+  var p = document.getElementById("input_error");
+  // For the X button on the Modal
+  tryagain.onclick = function() {
+    modal.style.display = "none";
+  }
+//For User to close the window when clicked anywhere
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+    try {
+        if(year == ""){ throw "Please Input Built Year. Range 1920-2021";}
+        else if(year < 1900) {throw "Input value for Built Year out of range.<br> Try Range 1920 - 2021"; }
+        else if(year > 2021) {throw "Input value for Built Year out of range.<br> Try Range 1920 - 2021"; }
+        else {var builtyear = year;}
+      }
+      catch(err3) {
+        p.innerHTML= err3;
+        modal.style.display="block";
+         
+      }
+  
+    try {
+        if(built == ""){throw "Please Input Built Size. Range 1-1000";} 
+        else if(built < 1){throw "Input value for Built Size too Low.<br> Try Range 1 - 1000";}
+        else if(built > 1000){throw "Input value for Built Size too High.<br> Try Range 1 - 1000";}
+        else {var builtsize = built;}
+      }
+      catch(err2) {
+        p.innerHTML= err2;
+        modal.style.display="block";
+      }
+
+     
+      try {
+        if(land == ""){throw "Please Input Land Size. Range 1-1500";}
+        else if(land < 1){throw "Input value for Land Size too Low.<br>Try Range 1 - 1500";}
+        else if(land > 1500){throw "Input value for Land Size too High.<br>Try Range 1 - 1500";}
+        else {var landsize = land;}
+      }
+      catch(err1) {
+         p.innerHTML= err1;
+         modal.style.display="block";
+      }
   
   console.log(bedroom, bathroom, carspace,landsize,builtsize,builtyear,suburb) 
 
@@ -78,33 +126,33 @@ d3.select("#submit_this").on("click",()=>{
       if (suburb == "perth") {
          myMap.setView([-31.9523 , 115.8613], 17);
          L.marker([-31.9523 , 115.8613])
-          .bindPopup("<h2>Perth</h2><hr><h3>Predicted Price Range:</h3><br> <b>" +data.result+ "</b>")
-          .addTo(myMap);
+          .bindPopup("<h2>Perth</h2><hr><h3>Estimation Range:</h3><br> <h3>" +data.result+ "</h3>")
+          .addTo(myMap).openPopup();
       } else if (suburb == "westp") {
          myMap.setView([-31.9463 , 115.8448], 17);
          L.marker([-31.9463 , 115.8448])
-          .bindPopup("<h2>West Perth</h2><hr><h3>Predicted Price Range:</h3><br> <b>" +data.result+ "</b>")
-          .addTo(myMap);
+          .bindPopup("<h2>West Perth</h2><hr><h3>Estimation Range:</h3><br> <h3>" +data.result+ "</h3>")
+          .addTo(myMap).openPopup();
       } else if (suburb == "eastp") {
           myMap.setView([-31.9608 , 115.8746], 17);
           L.marker([-31.9608 , 115.8746])
-          .bindPopup("<h2>East Perth</h2><hr><h3>Predicted Price Range:</h3><br> <b>" +data.result+ "</b>")
-          .addTo(myMap);
+          .bindPopup("<h2>East Perth</h2><hr><h3>Estimation Range:</h3><br> <h3>" +data.result+ "</h3>")
+          .addTo(myMap).openPopup();
       } else if (suburb == "northbridge") {
           myMap.setView([-31.9470 , 115.8574], 17);
           L.marker([-31.9470 , 115.8574])
-          .bindPopup("<h2>Northbridge</h2><hr><h3>Predicted Price Range:</h3><br> <b>" +data.result+ "</b>")
-          .addTo(myMap);
+          .bindPopup("<h2>Northbridge</h2><hr><h3>Estimation Range:</h3><br> <h3>" +data.result+ "</h3>")
+          .addTo(myMap).openPopup();
       } else if (suburb == "crawley") {
           myMap.setView([-31.9814 , 115.8205], 17);
           L.marker([-31.9814 , 115.8205])
-          .bindPopup("<h2>Crawley</h2><hr><h3>Predicted Price Range:</h3><br> <b>" +data.result+ "</b>")
-          .addTo(myMap);
+          .bindPopup("<h2>Crawley</h2><hr><h3>Estimation Range:</h3><br> <h3>" +data.result+ "</h3>")
+          .addTo(myMap).openPopup();
       } else if (suburb == "nedlands") {
           myMap.setView([-31.9810 , 115.8039], 17);
           L.marker([-31.9810 , 115.8039])
-          .bindPopup("<h2>Nedlands</h2><hr><h3>Predicted Price Range:</h3><br> <b>" +data.result+ "</b>")
-          .addTo(myMap);
+          .bindPopup("<h2>Nedlands</h2><hr><h3>Estimation Range:</h3><br> <h3>" +data.result+ "</h3>")
+          .addTo(myMap).openPopup();
     }
     //
 })
